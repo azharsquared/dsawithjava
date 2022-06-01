857746f3fa7c5455f1ce06370ed208ef406b7252
azharsqaured <azharsquared@gmail.com>
2022-06-01 23:54:23 +0530

    insertion sort

---
+++ src/com/azhar/dsa/array/InsertionSort.java
@@ -0,0 +1,27 @@
+package com.azhar.dsa.array;
+/*
+* in place
+* stable
+* more shifting
+* O(n^2)
+* */
+public class InsertionSort implements Sort{
+    public static void main(String[] args) {
+        sort();
+    }
+
+    private static void sort() {
+
+        int[] input = Sort.intArray;
+        for(int indexOfUnsortedArray = 1 ; indexOfUnsortedArray < input.length ; indexOfUnsortedArray++){
+
+            int element = input[indexOfUnsortedArray];
+            int indexToTraverse = indexOfUnsortedArray;
+            for (;indexToTraverse > 0 &&  element < input[indexToTraverse - 1]; indexToTraverse --){
+                input[indexToTraverse] = input[indexToTraverse - 1];
+            }
+            input[indexToTraverse] = element;
+        }
+        Sort.print(input);
+    }
+}
